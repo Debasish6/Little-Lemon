40852b0b7d6582cfb490b58d44d066fd0e37b548
Debasish6 <iamdebasishghosh6@gmail.com>
2024-05-02 15:44:19 +0530

    Successful

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Booking,Menu
 # Register your models here.
 admin.site.register(Booking)
 admin.site.register(Menu)
-=======
-
-# Register your models here.
->>>>>>> origin/main
