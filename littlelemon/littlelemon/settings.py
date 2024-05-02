ad6a0dd27f18d5340d4b6bd171392aef4e31955c
Debasish6 <iamdebasishghosh6@gmail.com>
2024-05-02 15:48:21 +0530

    Successful

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -55,11 +55,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
         'DIRS': ['templates'],
-=======
-        'DIRS': [],
->>>>>>> origin/main
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,7 +76,6 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
@@ -90,10 +85,6 @@ DATABASES = {
         'OPTIONS': {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
 }
-=======
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
->>>>>>> origin/main
     }
 }
 
@@ -134,13 +125,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-<<<<<<< HEAD
 STATICFILES_DIRS = [
     BASE_DIR,'static'
 ]
 
-=======
->>>>>>> origin/main
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
