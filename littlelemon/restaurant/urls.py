b0b713dc37d4f310883b352404fdbffee93a4ef3
Debasish6 <iamdebasishghosh6@gmail.com>
2024-05-05 06:35:09 +0530

    Commit Successfully Done

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index/',views.index, name='index'),
+    path('menu/',views.MenuItemsView.as_view()),
+    path('menu/<int:pk>',views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Menu,Booking
+from .serializers import MenuSerializer,BookingSerializer
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html',{})
+
+#It return all the menu items
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    # def get(self,request):
+    #     items = Menu.objects.all()
+    #     serialize_item = MenuSerializer(items,many = True)
+    #     return Response(serialize_item.data)
+    
+    # def post(self,request):
+    #     # title = request.POST.get('Title')
+    #     # price = request.POST.get('Price')
+    #     # inventory = request.POST.get('inventory')
+    #     # menu = Menu(
+    #     #     Title = title
+    #     #     Price = price
+    #     #     inventory = inventory
+    #     # )
+    #     serialized_item=MenuSerializer(data=request.data)
+    #     serialized_item.is_valid(raise_exception=True)
+    #     serialized_item.save()
+    #     return Response(serialized_item.data)
+
+#It returns single menu item based on id
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    # def get(self,request,pk):
+        
