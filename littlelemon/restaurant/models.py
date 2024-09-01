61da87b0a979de0cc7a9155e716a61dccedbb4f2
Debasish6 <iamdebasishghosh6@gmail.com>
2024-09-01 19:57:17 +0530

    Final commit

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from datetime import datetime
 
 # Create your models here.
 class Booking(models.Model):
     name = models.CharField(max_length=200)
     no_of_guests = models.IntegerField()
-    booking_date = models.DateTimeField()
+    booking_date = models.DateTimeField(default=datetime.now())
     
     def __str__(self):
         return self.name
